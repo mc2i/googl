2620b3fcf33e29e60f6f68ff2e510926bded6baa
M <refits.minxes0h@icloud.com>
2024-05-11 05:28:26 -0700

    parameterize

--- scripts/transfer_youtube_hist.ps1
+++ scripts/transfer_youtube_hist.ps1
@@ -1,7 +1,9 @@
-$dir = "$HOME/Downloads"
-$data = Get-Content -Path "$dir/Takeout/YouTube and YouTube Music/history/watch-history.json" | ConvertFrom-Json
+params(
+    [string]$takeout = $HOME
+)
+$data = Get-Content -Path "$takeout/YouTube and YouTube Music/history/watch-history.json" | ConvertFrom-Json
 $urls = $data.titleUrl | Select-Object -Unique                                                                           
 $reversedUrls = $urls.Clone()                 
 [Array]::Reverse($reversedUrls)
-$commands = for ($i = 0; $i -lt $reversedUrls.Count; $i++) {"$dir/yt-dlp.exe --quiet --cookies-from-browser chrome --simulate --mark-watched $($reversedUrls[$i])" + "rn" + "echo Progress: $i / $($(reversedUrls.Count)" }
-	$commands | Out-File -FilePath "$dir/youtube-hist-transfer-commands.bat"
+$commands = for ($i = 0; $i -lt $reversedUrls.Count; $i++) {"/yt-dlp.exe --quiet --cookies-from-browser chrome --simulate --mark-watched $($reversedUrls[$i])" + "rn" + "echo Progress: $i / $($reversedUrls.Count)" }
+	$commands | Out-File -FilePath "./youtube-hist-transfer-commands.bat"
